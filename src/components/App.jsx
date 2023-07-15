f266652f44dbc86d333eee3960ff1ad5b676c596
Mahendra <mahendradewangan195@gmail.com>
2023-07-15 20:44:29 +0530

    render each note separately

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(noteItem){
+    return (<Note 
+        key={noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+    />);
+}
 
 
 function App(){
     return <div>
        <Header />
-       <Note />
+       {/* <Note title="this is new title " content="this is new title content"/> */}
+       {notes.map((noteItem) => {
+            return (<Note 
+                key={noteItem.key}
+                title={noteItem.title}
+                content={noteItem.content}
+            />);
+        })};
        <Footer />
     </div>
 }
---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+    {
+        key: 1,
+        title: "Delegation",
+        content: "how many programmers does it take to change a light bulb??"
+    },
+    {
+        key: 2,
+        title: "loops",
+        content: "how to keep a programmer in the shower forever "
+    },
+    {
+        key: 3,
+        title: "OOPs",
+        content: "programming concept used in programming languages "
+    },
+    {
+        key: 4,
+        title: "Java",
+        content: "most hated programming language of all time. i dont know why?? "
+    },
+    {
+        key: 5,
+        title: "Python",
+        content: "best programming language for projects as it gives wide variety of libraries"
+    }
+];
+
+export default notes;
